ac89180a2aba60c967861dfd8b78ecf904d48c2f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-02-21 11:23:52 +0700

    Update ToDoAdapter.kt

--- app/src/main/java/com/wannaphong/todo/ToDoAdapter.kt
+++ app/src/main/java/com/wannaphong/todo/ToDoAdapter.kt
@@ -1,12 +1,26 @@
 package com.wannaphong.todo
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class ToDoAdapter : BaseAdapter() {
+class ToDoAdapter(context:Context, itemList: MutableList<ToDoItem>) : BaseAdapter() {
+    var items = itemList
+    var mInflater = LayoutInflater.from(context)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val objID = items.get(position).objID as String
+        val todoName = items.get(position).todoName as String
+        val statue = items.get(position).status as String
+
+        if(convertView == null){
+
+        }
+        else{
+
+        }
     }
 
     override fun getItem(position: Int): Any {
