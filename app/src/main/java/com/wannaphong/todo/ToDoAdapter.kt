9285f2477a37e9307a591528239bd4c10f8a2c76
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-02-21 11:33:39 +0700

    Update ToDoAdapter.kt

--- app/src/main/java/com/wannaphong/todo/ToDoAdapter.kt
+++ app/src/main/java/com/wannaphong/todo/ToDoAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.TextView
 
 class ToDoAdapter(context:Context, itemList: MutableList<ToDoItem>) : BaseAdapter() {
     var items = itemList
@@ -13,14 +16,27 @@ class ToDoAdapter(context:Context, itemList: MutableList<ToDoItem>) : BaseAdapte
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val objID = items.get(position).objID as String
         val todoName = items.get(position).todoName as String
-        val statue = items.get(position).status as String
+        val statue = items.get(position).status as Boolean
+        val view:View
+        val vh: ListRowHolder
 
         if(convertView == null){
-
+            view = mInflater.inflate(R.layout.list_item,parent,false)
+            vh = ListRowHolder(view)
+            view.tag = vh
         }
         else{
-
+            view = convertView
+            vh = view.tag as ListRowHolder
         }
+        vh.label.text = todoName
+        vh.checkbox.isChecked = statue
+        return view
+    }
+    private class ListRowHolder(row:View?){
+        val label = row!!.findViewById<TextView>(R.id.textView)
+        val checkbox = row!!.findViewById<CheckBox>(R.id.checkBox)
+        val deleteButton = row!!.findViewById<ImageButton>(R.id.imageButton)
     }
 
     override fun getItem(position: Int): Any {
